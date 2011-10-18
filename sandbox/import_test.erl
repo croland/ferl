d08bcd8f1aa72e22b72621ec5c0dcf163c60db8d
Chris Roland <croland@r6software.com>
2011-10-17 22:07:33 -0400

    Using map this time around

--- sandbox/import_test.erl
+++ sandbox/import_test.erl
@@ -1,34 +1,29 @@
+%% Copyright: Christopher Roland 2011
+%% 
+%% Description: Test file to determine how to import Yahoo finance data into CouchDB
+%% Usage:
+%% Call import_test:start_link() to start the gen_server
+%% Call import_test:run() to kick everything off
+
 -module(import_test).
 -behaviour(gen_server).
-
--export([init/1, run/0, start_link/0, handle_call/3, terminate/2]).
 -export([import_line/1]).
+-export([init/1, run/0, start_link/0, handle_call/3, terminate/2]).
 
-init([]) ->
-	application:start(sasl),
+init([]) -> application:start(sasl),
 	application:start(ibrowse),
-	%application:start(couchbeam),
 	couchbeam:start(),
 	Connection = couchbeam:server_connection(),
 	{ok, Db} = couchbeam:open_or_create_db(Connection, "test2", []),
 	{ok, Db}.
 
-start_link() ->
-	gen_server:start_link({local, import_test}, import_test, [], []).	
+start_link() -> gen_server:start_link({local, import_test}, import_test, [], []).	
 
-run() ->
-	{ok, Binary} = file:read_file("msft.csv"),
+run() -> {ok, Binary} = file:read_file("msft.csv"),
 	Lines = string:tokens(erlang:binary_to_list(Binary), "\n"),
-	import_lines(Lines).
+	lists:map(fun(L) -> import_line(L) end, Lines).
 
-import_lines([H|T]) ->
-	import_line(H),
-	import_lines(T);
-
-import_lines([]) -> done.
-
-import_line(Line) ->
-	gen_server:call(import_test, {import_line, Line}).	
+import_line(Line) -> gen_server:call(import_test, {import_line, Line}).	
 
 handle_call({import_line, Line}, _From, Db) ->
 	[Date, Open, High, Low, Close, Volume, AdjClose] = re:split(Line, "[,]", [{return, list}]),
