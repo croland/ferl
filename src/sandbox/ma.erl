a98e3646a99b6bf7f1ded4a4e36e10551c0f59cc
vagrant <vagrant@precise64.(none)>
2014-01-25 00:57:49 +0000

    Adding ability to specify file and trying to fix algo

--- src/sandbox/ma.erl
+++ src/sandbox/ma.erl
@@ -1,17 +1,17 @@
 -module(ma).
--export([run/0]).
+-export([run/1]).
 
 -record(tick, {date, open, high, low, close, vol, adjclose}).
 -record(account, {balance, name, transactions=[]}).
 
-run() ->
-	Ticks = parse(),
+run(File) ->
+	Ticks = parse(File),
 	Trades = calculate(Ticks, equal, []),
 	profitAndLoss(Trades, 1000),
 	Trades.
 
-parse() ->
-	{ok, Binary} = file:read_file("../../data/gld.csv"),
+parse(File) ->
+	{ok, Binary} = file:read_file(lists:concat(["../../data/", File, ".csv"])),
 	Lines = string:tokens(erlang:binary_to_list(Binary), "\r\n"),
 	Ticks = lists:map(fun(L) -> parse_line(L) end, Lines),
 	Ticks.
@@ -22,25 +22,24 @@ parse_line(Line) ->
 	Tick.
 
 calculate([H|T], Trend, Trades) -> 
-	Ticks9Day = lists:sublist(T, 9),	
-	Ticks18Day = lists:sublist(T, 18),	
-	Avg9Day = avgTicks(Ticks9Day, 0) / getLength(Ticks9Day),
-	Avg18Day = avgTicks(Ticks18Day, 0) / getLength(Ticks18Day),
-	AvgDirection = avgDirection(Avg9Day, Avg18Day),
+	TicksMa1Day = lists:sublist(T, 9),	
+	TicksMa2Day = lists:sublist(T, 18),	
+
+	AvgMa1Day = avgTicks(TicksMa1Day, 0) / getLength(TicksMa1Day),
+	AvgMa2Day = avgTicks(TicksMa2Day, 0) / getLength(TicksMa2Day),
+
+	AvgDirection = avgDirection(AvgMa1Day, AvgMa2Day),
 	TradeAction = tradeAction(Trend, AvgDirection),
+
 	NewTrades = trade(H, TradeAction) ++ Trades,
+
 	calculate(T, AvgDirection, NewTrades); 
+
 calculate([], Trend, Trades) -> Trades.
 
-avgDirection(Avg9Day, Avg18Day) when Avg9Day > Avg18Day -> above;
-avgDirection(Avg9Day, Avg18Day) when Avg9Day < Avg18Day -> below;
-avgDirection(Avg9Day, Avg18Day) when Avg9Day =:= Avg18Day -> equal.
-
-trade(Trade, TradeAction) when TradeAction =:= buy -> 
-	[{TradeAction, Trade#tick.date, Trade#tick.close}];
-trade(Trade, TradeAction) when TradeAction =:= sell -> 
-	[{TradeAction, Trade#tick.date, Trade#tick.close}];
-trade(Trade, TradeAction) when TradeAction =:= hold -> []. 
+avgDirection(Avg1Day, Avg2Day) when Avg1Day > Avg2Day -> above;
+avgDirection(Avg1Day, Avg2Day) when Avg1Day < Avg2Day -> below;
+avgDirection(Avg1Day, Avg2Day) when Avg1Day =:= Avg2Day -> equal.
 
 tradeAction(Trend, TickTrend) when Trend =:= TickTrend -> hold; 
 tradeAction(Trend, TickTrend) when Trend =:= below, TickTrend =:= equal -> hold;
@@ -49,6 +48,12 @@ tradeAction(Trend, TickTrend) when Trend =:= equal, TickTrend =:= above -> buy;
 tradeAction(Trend, TickTrend) when Trend =:= below, TickTrend =:= above -> buy;
 tradeAction(Trend, TickTrend) when Trend =:= equal, TickTrend =:= below -> sell;
 tradeAction(Trend, TickTrend) when Trend =:= above, TickTrend =:= below -> sell.
+
+trade(Trade, TradeAction) when TradeAction =:= buy -> 
+	[{TradeAction, Trade#tick.date, Trade#tick.close}];
+trade(Trade, TradeAction) when TradeAction =:= sell -> 
+	[{TradeAction, Trade#tick.date, Trade#tick.close}];
+trade(Trade, TradeAction) when TradeAction =:= hold -> []. 
 
 getLength([H|T]) -> 1 + length(T);
 getLength([]) -> 1.
@@ -60,16 +65,20 @@ addTicks([H|T], Acc) -> addTicks(T, H#tick.close + Acc);
 addTicks([], Acc) -> Acc.
 
 profitAndLoss([H|T], Total) -> 
-	NewTotal = getTotal(H, Total),
+	[PrevTrade|Tail] = T,
+	NewTotal = getTotal(H, PrevTrade, Total),
 	io:format("~f~n", [NewTotal]),
 	profitAndLoss(T, NewTotal);
 profitAndLoss([], Total) -> Total.
 
-getTotal(Trade, Total) -> 
-	case Trade of 
-		{buy, Date, Close} -> Total - Close;
-		{sell, Date, Close} -> Total + Close;
-		_Else -> Total 
-	end.
+getTotal(Trade, PrevTrade, Total) -> 
+	{PrevAction, PrevDate, PrevClose} = PrevTrade,
+	{Action, Date, Close} = Trade,
+	(Total + (Close - PrevClose)).
+	%% case Trade of 
+	%%	{buy, Date, Close} -> Total - PrevClose - Close;
+	%%		{sell, Date, Close} -> Total + PrevClose - Close;
+	%%		_Else -> Total 
+	%end.
 
 
