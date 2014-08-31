316dcc74d1631b8189a7eb06d34001e3a7f85c5d
Chris Roland <chris@rolandtrading.com>
2014-08-31 12:18:11 -0400

    Adding variance

--- src/lib/calcs.erl
+++ src/lib/calcs.erl
@@ -8,12 +8,14 @@ cov([], Xtotal, Ytotal, XYtotal, Count) ->
 cov([{X,Y}|T], Xtotal, Ytotal, XYtotal, Count) ->
 	cov(T, Xtotal+X, Ytotal+Y, XYtotal+(X*Y), Count+1).
 
-mean([], Total, Count) ->
-	Total/Count;
-mean([H|T], Total, Count) ->
-	mean(T, Total+H, Count+1).
-		
-cov_test() ->
-	?assert(cov([{1,2}, {3,4}], 0, 0, 0, 0) =:= 1.0).		
-mean_test() ->
-	?assert(mean([1,2], 0, 0) =:= 1.5).
+mean([], Total, Count) -> Total/Count;
+mean([H|T], Total, Count) -> mean(T, Total+H, Count+1).
+
+var(List) -> var_calc(List, mean(List, 0, 0), 0, 0).
+var_calc([], _, Total, Count) -> Total/Count;
+var_calc([H|T], Mean, Total, Count) ->
+	var_calc(T, Mean, Total+math:pow(H-Mean, 2), Count+1).
+
+cov_test() -> ?assert(cov([{1,2}, {3,4}], 0, 0, 0, 0) =:= 1.0).		
+mean_test() -> ?assert(mean([1,2], 0, 0) =:= 1.5).
+var_test() -> ?assert(var([110,200,83]) =:= 2502.0).
