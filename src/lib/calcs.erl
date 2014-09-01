1512fb6fba0a67c6cc014c4fb4cb137a69b5e20c
Chris Roland <chris@rolandtrading.com>
2014-08-31 21:07:19 -0400

    Adding standard deviation

--- src/lib/calcs.erl
+++ src/lib/calcs.erl
@@ -16,6 +16,9 @@ var_calc([], _, Total, Count) -> Total/Count;
 var_calc([H|T], Mean, Total, Count) ->
 	var_calc(T, Mean, Total+math:pow(H-Mean, 2), Count+1).
 
+std(List) -> math:sqrt(var(List)).
+
 cov_test() -> ?assert(cov([{1,2}, {3,4}], 0, 0, 0, 0) =:= 1.0).		
 mean_test() -> ?assert(mean([1,2], 0, 0) =:= 1.5).
 var_test() -> ?assert(var([110,200,83]) =:= 2502.0).
+std_test() -> ?assert(std([110,200,83]) =:= 50.0199960015992).
